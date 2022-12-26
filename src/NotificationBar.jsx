e5d08e9ffb0a4d9ab350f9511b2a410a3bb7c168
fumifumifu <masafumi.nozawa.q@gmail.com>
2022-12-26 02:10:22 -0800

    fix a bug on the notification bar

--- src/NotificationBar.jsx
+++ src/NotificationBar.jsx
@@ -7,7 +7,7 @@ function NotificationBar() {
     const fetchNews = async () => {
       const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${'8cc4e45e8b4a4f75a6c0012522a3463e'}`);
       const data = await response.json();
-      setNews(data.articles && data.articles.slice(0, 100));
+      setNews(data.articles.slice(0, 10));
     };
 
     fetchNews();
@@ -15,12 +15,12 @@ function NotificationBar() {
 
   return (
     <div className="news">
-     {news && news.map(item => (
-  <div key={item.title} style={{ display: 'flex', alignItems: 'center', backgroundColor:'#ff4343', color:'#ffffff',height:'32px'}}>
-    <span>{new Date(item.publishedAt).toDateString()}&nbsp;</span> 
-    <span style={{ marginRight: '10px' }}>{item.title}</span>
-  </div>
-))}
+      {news.map(item => (
+        <div key={item.title} style={{ display: 'flex', alignItems: 'center', backgroundColor:'#ff4343', color:'#ffffff',height:'32px'}}>
+          <span>{new Date(item.publishedAt).toDateString()}&nbsp;</span> 
+          <span style={{ marginRight: '10px' }}>{item.title}</span>
+        </div>
+      ))}
     </div>
   );
 }
