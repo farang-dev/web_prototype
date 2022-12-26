4f2bc7ea014968fae5097b990a778eea9bb1220d
fumifumifu <masafumi.nozawa.q@gmail.com>
2022-12-26 02:21:02 -0800

    fix the bug on the notification bar

--- src/NotificationBar.jsx
+++ src/NotificationBar.jsx
@@ -5,13 +5,20 @@ function NotificationBar() {
 
   useEffect(() => {
     const fetchNews = async () => {
-      const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=8cc4e45e8b4a4f75a6c0012522a3463e`);
-      const data = await response.json();
-      setNews(data.articles.slice(0, 10));
+      try {
+        const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=8cc4e45e8b4a4f75a6c0012522a3463e`);
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        const data = await response.json();
+        setNews(data.articles.slice(0, 10));
+      } catch (error) {
+        console.error(error);
+      }
     };
-
     fetchNews();
   }, []);
+  
 
   return (
     <div className="news">
